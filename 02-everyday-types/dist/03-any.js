445ca14ac5728eb4a2c261bbe78925196fc7fb23
sunyoung <cn.sunyoung@gmail.com>
2022-08-14 21:27:21 +0800

    add 03-any.ts

---
+++ 02-everyday-types/dist/03-any.js
@@ -0,0 +1,9 @@
+"use strict";
+let obj = {
+    x: 0
+};
+obj.foo();
+obj();
+obj.y = 2;
+obj = "ts";
+const n = obj;
---
+++ 02-everyday-types/src/03-any.ts
@@ -0,0 +1,9 @@
+let obj: any = {
+  x: 0
+} 
+
+obj.foo();
+obj();
+obj.y = 2;
+obj = "ts";
+const n: number = obj;
