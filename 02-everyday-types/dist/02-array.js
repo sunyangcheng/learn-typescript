49a389e00d3571c06d0da8dcfa3bca1cdb39956c
sunyoung <cn.sunyoung@gmail.com>
2022-08-14 21:23:59 +0800

    add 02-array.ts

---
+++ 02-everyday-types/dist/02-array.js
@@ -0,0 +1,6 @@
+"use strict";
+let arr = [1, 2, 3];
+// arr = ["a"]; // 不能将类型“string”分配给类型“number”。
+let arr2 = [1, 2, 3];
+arr = [];
+// arr = ["a"]; // 不能将类型“string”分配给类型“number”。
---
+++ 02-everyday-types/src/02-array.ts
@@ -0,0 +1,5 @@
+let arr: number[] = [1, 2, 3];
+// arr = ["a"]; // 不能将类型“string”分配给类型“number”。
+let arr2: Array<number> = [1, 2, 3];
+arr = [];
+// arr = ["a"]; // 不能将类型“string”分配给类型“number”。
