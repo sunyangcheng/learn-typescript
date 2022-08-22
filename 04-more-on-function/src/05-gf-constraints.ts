e82f584ff0b1280e83c0fc8a4d03a71102d1cf9a
sunyoung <cn.sunyoung@gmail.com>
2022-08-22 21:25:38 +0800

    add 04/more-on-function/05-gf-constraints.ts

---
+++ 04-more-on-function/src/05-gf-constraints.ts
@@ -0,0 +1,11 @@
+function longest<T extends { length: number }>(x: T, y: T) {
+  if (x.length > y.length) {
+    return x;
+  } else {
+    return y;
+  }
+}
+
+const longArr = longest([1,2,3], [4,5]); // "hello" 
+const longStr = longest("hello", "ts"); // "ts" 
+const longNum = longest(1, 2); // 类型“number”的参数不能赋给类型“{ length: number; }”的参数。
